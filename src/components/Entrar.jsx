cf57e91f624be003d848280cbbfe88c87e7dd8a2
Guilherme Rabelo <guilhermerabelo699@gmail.com>
2023-10-15 12:24:48 -0300

    Feat/Criando a funcionalidade de cadastro de usuario

--- src/components/Entrar.jsx
+++ src/components/Entrar.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Login } from '../components/Login';
 import { Signin } from '../components/Singin';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Entrar() {
-    const [ telaLogin, setTelaLogin ] = useState(true);
+    const [ telaLogin, setTelaLogin ] = useState(false);
     return (
         <>
             <Login 
@@ -14,6 +16,7 @@ export default function Entrar() {
                 mostrarTelaLogin={telaLogin}
                 mudarTela={setTelaLogin}
             />
+            <ToastContainer pauseOnHover={false} />
         </>
     )
 }
--- src/components/Singin.jsx
+++ src/components/Singin.jsx
@@ -1,6 +1,9 @@
 import './css/signin.css'
 import logo from '../assets/logo.png'
 import PropTypes from 'prop-types';
+import { api } from '../lib/api';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 Signin.propTypes = {
     mostrarTelaLogin: PropTypes.bool,
@@ -8,6 +11,87 @@ Signin.propTypes = {
 };
 
 export function Signin({ mostrarTelaLogin, mudarTela }) {
+    const [ erroNoPrenchimento, setErroNoPreenchimento ] = useState(false);
+    const [ validacaoFormulario, setValidacaoFormulario ] = useState({
+        nome: false,
+        email: false,
+        senha: false,
+        confirmaSenha: false
+    });
+    const [ formSign, setFormSign ] = useState({
+        nome: '',
+        email: '',
+        senha: '',
+        confirmaSenha: ''
+    });
+
+    const handleChangeForm = (event) => {
+        const { id, value } = event.target;
+        const form = {...formSign};
+        form[id] = value;
+
+        setFormSign(form)
+    }
+
+    const cadastraUsuario = async () => {
+        const campos = Object.keys(validacaoFormulario);
+
+        const validacao = {...validacaoFormulario};
+        let erroDePrenchimento = false;
+        campos.forEach((campo) => {
+            if(formSign[campo] == '') {
+                validacao[campo] = true;
+                erroDePrenchimento = true;
+            } else {
+                validacao[campo] = false;
+            }
+        });
+        setValidacaoFormulario(validacao);
+
+        if(erroDePrenchimento) {
+            toast.error('Erro no preenchimento do formulário', {
+                autoClose: 2000,
+            });
+            setErroNoPreenchimento(false);
+
+            return
+        }
+
+        if(formSign.senha !== formSign.confirmaSenha) {
+            toast.error('As senhas digitadas não correspondem', {
+                autoClose: 2000,
+            });
+            return
+        }
+
+        try {
+            const response = await api.post(
+                '/singin',
+                {
+                    Nome: formSign.nome,
+                    Email: formSign.email,
+                    Senha: formSign.senha
+                }
+            )
+
+            if(response.data.code == 400) {
+                toast.error('O email digitado já está em uso', {
+                    autoClose: 2000,
+                });
+                return
+            }
+            mudarTela(!mostrarTelaLogin)
+            toast.success('Usuário cadastrado com sucesso!', {
+                autoClose: 2000,
+            });
+        } catch(err) {
+            console.log(err)
+            toast.error('Erro ao cadastrar usuário', {
+                autoClose: 2000,
+            });
+        }
+    }
+
     if(!mostrarTelaLogin) {
         return (
             <div className="page-signin">
@@ -36,11 +120,11 @@ export function Signin({ mostrarTelaLogin, mudarTela }) {
                                 <img id="logo-signin" className="img-logo" src={logo} alt="Logo da plataforma" />
                                 <h2 className="text-tituloSignin">Cadastre-se no Finanças Web</h2>
                             </div>
-                            <input id="input-nome" type="text" placeholder="Nome e sobrenome"/>
-                            <input id="input-email" type="text" placeholder="Email"/>
-                            <input id="input-senha" type="password" placeholder="Senha"/>
-                            <input id="input-senhaConfirmacao" type="password" placeholder="Confirme sua senha"/>
-                            <button className="btn-cadastrar">Cadastrar</button>
+                            <input id="nome" className={validacaoFormulario['nome'] ? `erro` : ''} type="text" placeholder="Nome e sobrenome" onChange={handleChangeForm}/>
+                            <input id="email" className={validacaoFormulario['email'] ? `erro` : ''} type="text" placeholder="Email" onChange={handleChangeForm}/>
+                            <input id="senha" className={validacaoFormulario['senha'] ? `erro` : ''} type="password" placeholder="Senha" onChange={handleChangeForm}/>
+                            <input id="confirmaSenha" className={validacaoFormulario['confirmaSenha'] ? `erro` : ''} type="password" placeholder="Confirme sua senha" onChange={handleChangeForm}/>
+                            <button className="btn-cadastrar" onClick={cadastraUsuario}>Cadastrar</button>
                         </div>
                     </div>
                     <div className="area-footer">
