c2c1e02394ef6c34e011cf8d483f5d8020df41fb
Guilherme Rabelo <Guilherme Rabelo (Pessoal)>
2024-01-14 18:59:46 -0300

    Feat/Configurando componente de acesso rápido das movimentações

--- src/components/MovimentacoesAcessoRapido.jsx
+++ src/components/MovimentacoesAcessoRapido.jsx
@@ -1,12 +1,51 @@
 import './css/MovimentacoesAcessoRapido.css'
+import Cookies from 'js-cookie';
+import { api } from '../lib/api';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { BsArrowRight } from 'react-icons/bs';
 
 export default function MovimentacoesAcessoRapido() {
+    const [ movimentos, setMovimentos ] = useState([]);
+
+    const email = decodeURIComponent(Cookies.get('userEmail'));
+    useEffect(() => {
+        async function getMovimentos() {
+            try {
+                const response = await api.get(`/listaMovimentos/${email}`)
+                const data = response.data.data;
+
+                setMovimentos(data)
+            } catch(error) {
+                console.log(error)
+            }
+        }
+        getMovimentos()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [email])
+
     return (
         <>
             <div className="movimentacoes container-acessoRapido">
-                <h4>Últimas movimentações</h4>
+                <div className="movimentacoes-titulo">
+                    <h4>Últimas movimentações</h4>
+                </div>
                 <div className="movimentacoes-content">
-                    
+                    {movimentos.map((movimento, i) => {
+                        return (
+                            <div key={i} className="item-movimento">
+                                <div className="area-etapasExtrato"><div className="checkpoint"></div><div className="route"></div></div>
+                                <div className="area-descricaoMovimento">{movimento.descricao}<span className="acessoRapido-dataMovimento">{movimento.data}</span></div>
+                                <div className={`area-valorMovimento ${movimento.tipo == 'Receita' ? 'receita' : 'despesa'}`}>{movimento.valor}{`${movimento.tipo == 'Receita' ? ' C' : ' D'}`}</div>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="movimentacoes-seguirLink">
+                    <Link to="/movimentacoes" className='link-movimentacoes'>
+                        <BsArrowRight />
+                    </Link>
                 </div>
             </div>
         </>
